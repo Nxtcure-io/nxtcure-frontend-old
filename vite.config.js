40ae5b21f966589eca141993f9d79e84eb26d423
Ishita Kapoor <ishitakapoor12612@gmail.com>
2025-05-17 00:15:03 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/nxtcure-frontend/",
   plugins: [react(), tailwindcss()],
+  base: "/nxtcure-frontend",
 });
