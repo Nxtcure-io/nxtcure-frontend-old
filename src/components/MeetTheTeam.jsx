16399de2268dfacb74aa112435c359c695ba7dd9
nxtcure-dev <dev@nxtcure.com>
2025-06-12 09:56:14 -0400

    refactoring leader info

--- src/components/MeetTheTeam.jsx
+++ src/components/MeetTheTeam.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { motion, useInView, AnimatePresence } from "framer-motion";
-import ishitaPic from "../assets/ishita.jpg";
 import alessandroPic from "../assets/alessandro.jpg";
 
 const cardVariants = {
@@ -58,7 +57,7 @@ export default function MeetTheTeamSection() {
                       />
                       <div className="text-left">
                         <h4 className="font-bold">Alessandro Hammond</h4>
-                        <p className="text-sm text-gray-400">Co-Founder</p>
+                        <p className="text-sm text-gray-400">CEO</p>
                       </div>
                     </div>
                     <p className="text-justify text-gray-600 text-sm">
@@ -110,81 +109,6 @@ export default function MeetTheTeamSection() {
           </AnimatePresence>
         </div>
 
-        {/* Ishita */}
-        <div ref={ref2} className="h-full">
-          <AnimatePresence mode="wait">
-            {inView2 && (
-              <motion.div
-                key="ishita"
-                variants={cardVariants}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                className="bg-white p-6 rounded-lg border border-gray-400 max-w-md w-full h-full"
-                style={{ boxShadow: "1px 2px 5px gray" }}
-              >
-                <div className="flex flex-col justify-between h-full">
-                  <div>
-                    <div className="flex items-center mb-4">
-                      <img
-                        src={ishitaPic}
-                        alt="Ishita"
-                        className="w-20 h-20 rounded-full mr-4"
-                      />
-                      <div className="text-left">
-                        <h4 className="font-bold">Ishita Kapoor</h4>
-                        <p className="text-sm text-gray-400">Co-Founder</p>
-                      </div>
-                    </div>
-                    <p className="text-justify text-gray-600 text-sm">
-                      Ishita Kapoor is a technologist, 2x co-founding
-                      impact-driven ventures that merge tech innovation with
-                      strategic business insight. A Schwarzman Scholar, startup
-                      board member at IIT Roorkee, she has earned accolades from
-                      Microsoft, Google, and UNICEF, with 30+ national and
-                      international hackathon wins. Ishita has presented her
-                      work to Microsoft CEO Satya Nadella and now closely works
-                      with Microsoft as one of their PR partners. She was also
-                      awarded India’s Best Research Paper of the Year in 2023,
-                      recognized through Scopus-indexed journals.
-                    </p>
-                  </div>
-                  <div className="mt-4 text-left">
-                    <a
-                      href="https://www.linkedin.com/in/itsishakapoor/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="Ishita LinkedIn"
-                      className="inline-block"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        className="w-6 h-6 text-gray-600 hover:text-blue-700"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M16 8a6 6 0 016 6v6h-4v-6a2 2 0 00-4 0v6h-4v-6a6 6 0 016-6zM2 9h4v12H2z"
-                        />
-                        <circle
-                          cx="4"
-                          cy="4"
-                          r="2"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                        />
-                      </svg>
-                    </a>
-                  </div>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
       </div>
     </section>
   );
