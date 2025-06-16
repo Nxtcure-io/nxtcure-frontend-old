044ef857290a0b804490f3f29413725e51b7cc4e
nxtcure-dev <dev@nxtcure.com>
2025-06-15 20:20:34 -0400

    testing style for waitlist banner

--- src/components/WaitlistBanner.jsx
+++ src/components/WaitlistBanner.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function WaitlistBanner() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen px-6 py-6 text-center bg-[radial-gradient(circle_at_center,_#F1E2FE_10%,_#F3ECFF_30%,_#EEF7FF_80%)]">
+    <section className="flex flex-col items-center justify-center min-h-screen px-6 py-6 text-center bg-[radial-gradient(circle_at_center,_#800080_10%,_#020202_30%,_#0f0f0f_80%)]">
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb_5tHzHN3NrhJFKpRhEhLkQIDQrXdH7jXGKK-PZrt4KBaAg/viewform?usp=sharing&ouid=105836010722188729845">
       <motion.div
         className="max-w-3xl"
@@ -19,7 +19,7 @@ export default function WaitlistBanner() {
           viewport={{ once: false, amount: 0.3 }}
           transition={{ delay: 0.1, duration: 0.6 }}
         >
-          <span className="cormorant text-black bg-clip-text">
+          <span className="cormorant text-white bg-clip-text">
 	  	Join Our Waitlist
           </span>
         </motion.h1>
